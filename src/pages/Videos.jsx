8abfc1c024fbbd2ee1f47cd77c38c9e1cc8fee4c
chefkuna <sukhyun94@naver.com>
2022-11-13 21:50:40 +0900

    VideoCard title mockdata

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,7 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { useParams } from "react-router-dom";
+import { search } from "../api/yotube";
+import VideoCard from "../components/VideoCard";
 
 export default function Videos() {
   const { keyword } = useParams();
-  return <div>Videos {keyword ? `👀${keyword}` : "🔥HOT TREND"}</div>;
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(["videos", keyword], () => search(keyword));
+  return (
+    <>
+      <div>Videos {keyword ? `👀${keyword}` : "🔥HOT TREND"}</div>;
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong ❓</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
